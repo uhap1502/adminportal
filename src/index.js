490e283ba5001dd73d25005d926c68a2f288befe
uhap1502 <59677784+uhap1502@users.noreply.github.com>
2020-01-09 10:47:29 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ store.dispatch(fetchAllPosts());
 ReactDOM.render(<Provider store={store}> <App />
 </Provider>, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
+// If you want your app to work and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 
